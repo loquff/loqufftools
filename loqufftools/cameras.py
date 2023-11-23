785829a9a899eb8978a04823c410b16d89f8cf1a
LOQUFF <marcosgildeoliveira@gmail.com>
2023-11-23 10:53:10 -0300

    minor changes

--- loqufftools/cameras.py
+++ loqufftools/cameras.py
@@ -1,10 +1,18 @@
+import ctypes as C
+import os
+from tqdm import tqdm
+from loqufftools.hdf5_utils import *
+from slmcontrol import *
+import configparser
+import h5py
 from abc import ABC, abstractmethod
 import numpy as np
 from PIL import Image
 from multimethod import multimethod
 
+
 class Camera(ABC):
-    def __init__(self,resX,resY):
+    def __init__(self, resX, resY):
         self.resX = resX
         self.resY = resY
 
@@ -16,39 +24,44 @@ class Camera(ABC):
     def close(self):
         pass
 
+
 class TestCamera(Camera):
-    def __init__(self,resX,resY):
-        super().__init__(resX,resY)
-    
+    def __init__(self, resX, resY):
+        super().__init__(resX, resY)
+
     @multimethod
     def capture(self, saving_path: str):
-        image = Image.fromarray(np.random.randint(0,255,size=(self.resY,self.resX),dtype='uint8'))
+        image = Image.fromarray(np.random.randint(
+            0, 255, size=(self.resY, self.resX), dtype='uint8'))
         image.save(saving_path)
 
     @multimethod
     def capture(self):
-        return np.random.randint(0,255,size=(self.resY,self.resX),dtype='uint8')
+        return np.random.randint(0, 255, size=(self.resY, self.resX), dtype='uint8')
 
     def close(self):
         pass
+
 
 """
 Imaging Source Cameras
 """
 
-import h5py
-import configparser
-from slmcontrol import *
-from hdf5_utils import *
-from tqdm import tqdm
-import os
 
 try:
     # Import PyhtonNet
     import sys
+    import os
     import clr
-    # Load IC Imaging Control .NET 
-    sys.path.append("C:/Users/marco/Documents/IC Imaging Control 3.5/redist/dotnet/x64")
+    # Load IC Imaging Control .NET
+    ic35path = os.getenv('IC35PATH')
+    if ic35path is None:
+        raise OSError(
+            """The 'IC35PATH' enviroment variable seems not to be set. \n
+            On Windows, it should point to the full path of the folder Documents/IC Imaging Control 3.5. \n
+            You can set this variable manually, but remember to restart your computer.""")
+    else:
+        sys.path.append(ic35path + "/redist/dotnet/x64")
     clr.AddReference('TIS.Imaging.ICImagingControl35')
     clr.AddReference('System')
 
@@ -61,11 +74,10 @@ except:
         Check https://github.com/TheImagingSource/IC-Imaging-Control-Samples/tree/master/Python/Python%20NET for more information.
         """))
 
-import ctypes as C
 
 class ImagingSourceCamera(Camera):
-    def __init__(self,resX,resY):
-        super().__init__(resX,resY)
+    def __init__(self, resX, resY):
+        super().__init__(resX, resY)
         self.imaging_control = TIS.Imaging.ICImagingControl()
 
         # Create the sink for snapping images on demand.
@@ -76,7 +88,7 @@ class ImagingSourceCamera(Camera):
 
         # Try to open the last used video capture device.
         try:
-            self.imaging_control.LoadDeviceStateFromFile("device.xml",True)
+            self.imaging_control.LoadDeviceStateFromFile("device.xml", True)
             if self.imaging_control.DeviceValid is True:
                 self.imaging_control.LiveStart()
 
@@ -86,34 +98,35 @@ class ImagingSourceCamera(Camera):
                 self.imaging_control.SaveDeviceStateToFile("device.xml")
                 self.imaging_control.LiveStart()
             pass
-    
+
     @multimethod
-    def capture(self, roi = None):
+    def capture(self, roi=None):
         image = self.imaging_control.Sink.SnapSingle(TimeSpan.FromSeconds(5))
-        imgcontent = C.cast(image.GetIntPtr().ToInt64(), C.POINTER(C.c_ubyte * image.FrameType.BufferSize))
-        result = np.ndarray(buffer = imgcontent.contents,
-                            dtype = np.uint8,
-                            shape = (image.FrameType.Height,
-                                    image.FrameType.Width) )
-        
+        imgcontent = C.cast(image.GetIntPtr().ToInt64(), C.POINTER(
+            C.c_ubyte * image.FrameType.BufferSize))
+        result = np.ndarray(buffer=imgcontent.contents,
+                            dtype=np.uint8,
+                            shape=(image.FrameType.Height,
+                                   image.FrameType.Width))
+
         if roi == None:
             return result
         else:
-            return result[slice(roi[0],roi[1]),slice(roi[2],roi[3])]
-    
+            return result[slice(roi[0], roi[1]), slice(roi[2], roi[3])]
+
     @multimethod
-    def capture(self, saving_path: str, roi = None):
+    def capture(self, saving_path: str, roi=None):
         image = self.imaging_control.Sink.SnapSingle(TimeSpan.FromSeconds(5))
         TIS.Imaging.FrameExtensions.SaveAsBitmap(image, saving_path)
 
-    def loop_capture_as_hdf5(self, saving_path, slm, config_path, source_key, saving_key,roi=None, calculate_mean=False):
+    def loop_capture_as_hdf5(self, saving_path, slm, config_path, source_key, saving_key, roi=None, calculate_mean=False):
         try:
             with h5py.File(saving_path, 'a') as file:
                 check_safety(file, saving_key)
 
                 config = configparser.ConfigParser()
                 config.read(config_path)
-                x,y = build_grid(config_path)
+                x, y = build_grid(config_path)
 
                 max = config['slm'].getint('max')
                 xoffset = config['input'].getint('xoffset')
@@ -122,36 +135,38 @@ class ImagingSourceCamera(Camera):
                 xperiod = config['grating'].getfloat('xperiod')
                 yperiod = config['grating'].getfloat('yperiod')
 
-                input = hg(x,y,0,0,waist)
+                input = hg(x, y, 0, 0, waist)
 
                 fields = file[source_key]
                 if roi == None:
-                    result = np.zeros((fields.shape[0],self.resY,self.resX), dtype='uint8')
+                    result = np.zeros(
+                        (fields.shape[0], self.resY, self.resX), dtype='uint8')
                 else:
-                    result = np.zeros((fields.shape[0],roi[1]-roi[0],roi[3]-roi[2]), dtype='uint8')
-
+                    result = np.zeros(
+                        (fields.shape[0], roi[1]-roi[0], roi[3]-roi[2]), dtype='uint8')
 
                 for n, desired in tqdm(enumerate(fields)):
-                    holo = generate_hologram(desired,input,x,y,max,xperiod,yperiod,xoffset,yoffset)
+                    holo = generate_hologram(
+                        desired, input, x, y, max, xperiod, yperiod, xoffset, yoffset)
                     slm.updateArray(holo)
 
-                    result[n,:,:] = self.capture(roi=roi)
+                    result[n, :, :] = self.capture(roi=roi)
 
-                file.create_dataset(saving_key, data = result)
+                file.create_dataset(saving_key, data=result)
 
             if calculate_mean:
                 directory_path, old_filename = os.path.split(saving_path)
                 new_path = os.path.join(directory_path, 'mean_' + old_filename)
                 with h5py.File(new_path, 'a') as file:
-                    check_safety(file,saving_key)
-                    file[saving_key] = np.mean(result,axis=0)
+                    check_safety(file, saving_key)
+                    file[saving_key] = np.mean(result, axis=0)
 
-        except Exception as e: 
+        except Exception as e:
             print(e)
         finally:
             self.close()
             file.close()
-    
+
     def close(self):
         self.imaging_control.LiveStop()
         self.imaging_control.Dispose()
